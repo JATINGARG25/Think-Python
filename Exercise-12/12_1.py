09655b2bf1b6a1477628059eb3cff894c22a6249
JATINGARG25 <gargjatin519@gmail.com>
2024-01-13 21:00:46 +0530

    starting of tuples

---
+++ Exercise-12/12_1.py
@@ -0,0 +1,13 @@
+# example of tuples
+# the main difference between a list or a tuple is that list are mutable while tupe are immutable
+# mutable means -- we can change the values of mutable things such as list , dictionary
+# immutable means -- we can not change the values of immutable things such as tuple , string , integers
+
+t = 'a',2,'c','cat','dog'
+a = 'a',                    # for a tuple which contain only a single element
+
+print(t)
+print(type(t))
+
+print(a)
+print(type(a))
