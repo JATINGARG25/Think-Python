73cdb7d49021f629c692780aec4fb9ca385e0b3e
JATINGARG25 <gargjatin519@gmail.com>
2024-01-14 13:14:42 +0530

    made a new directory

--- Exercise-13/13_4.py
+++ Exercise-13/13_4.py
@@ -1,4 +1,4 @@
-import string
+from string import *
 
 def process_file(filename):
     hist = dict()
@@ -9,11 +9,35 @@ def process_file(filename):
 
 def process_line(line, hist):
     line = line.replace('-', ' ')
-    
+
     for word in line.split():
-        word = word.strip(string.punctuation + string.whitespace)
+        word = word.strip(punctuation + whitespace)
         word = word.lower()
 
         hist[word] = hist.get(word, 0) + 1
 
-hist = process_file('emma.txt')
+def total_words(hist):
+    return sum(hist.values())
+
+def different_words(hist):
+    return len(hist)
+
+def most_common(hist):
+    t = []
+    for key, value in hist.items():
+        t.append((value, key))
+    t.sort(reverse=True)
+    return t
+
+def print_most_common(hist, num=10):
+    t = most_common(hist)
+    print('The most common words are:')
+    for freq, word in t[:num]:
+        print(word, '\t', freq)
+        
+hsit = process_file("word.txt")
+print(hsit)
+# print(a)
+
+
+
