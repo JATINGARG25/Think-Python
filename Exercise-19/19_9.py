decef05793e31479f8b9de534a62499782109ce2
JATINGARG25 <gargjatin519@gmail.com>
2024-01-19 19:53:45 +0530

    add some file

--- Exercise-19/19_9.py
+++ Exercise-19/19_9.py
@@ -1,5 +1,14 @@
 from swampy.Gui import *
 
+def set_color(color):
+    mb.config(text=color)
+    # print(color)
+
 g = Gui()
 g.title("Interface")
+g.la("Select a Color:")
+colors = ["red","green","blue","yellow"]
+mb = g.mb(text=colors[0])
+for color in colors:
+    g.mi(mb, text=color, command=Callable(set_color, color))
 g.mainloop()
